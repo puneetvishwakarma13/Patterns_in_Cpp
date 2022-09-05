2ecb31153c958091d0d7cd516eb6f26bc217cb0b
puneetvishwakarma13 <56038652+puneetvishwakarma13@users.noreply.github.com>
2022-09-06 05:25:54 +0530

    Add files via upload

---
+++ pattern5.cpp
@@ -0,0 +1,36 @@
+// pattern5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Enter Size:
+//3
+//1 2 3
+//4 5 6
+//7 8 9
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter Size:" << endl;
+    cin >> n;
+    int count = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << count << " ";
+            count++;
+        }
+        
+        cout << endl;
+    }
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
